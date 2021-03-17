ad2219d4dedaf3ed6cc64f4f69df4ee569e2032e
Kanui <marley.kanui@gmail.com>
2021-03-17 14:07:16 -0500

    django-react-music-controller - global commit 02 - tested url paths and dev server

--- django_react_music_controller/django_react_music_controller/wsgi.py
+++ django_react_music_controller/django_react_music_controller/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_react_kanui project.
+WSGI config for django_react_music_controller project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_react_kanui.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_react_music_controller.settings')
 
 application = get_wsgi_application()
