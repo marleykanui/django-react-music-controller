be9cd4c639274412f99e973479a1a1547e23922e
Kanui <marley.kanui@gmail.com>
2021-03-18 13:14:13 -0500

    django-react-music-controller - global commit 09 - set up user session controller in django to redirect on root page load

--- django_react_music_controller/api/views.py
+++ django_react_music_controller/api/views.py
@@ -4,6 +4,7 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -32,22 +33,24 @@ class GetRoom(APIView):
 
 
 class JoinRoom(APIView):
-        lookup_url_kwarg = 'code'
-        def post(self, request, format=None):
-            if not self.request.session.exists(self.request.session.session_key):
-                self.request.session.create()
-            
-            code = request.data.get(self.lookup_url_kwarg)
-            if code != None:
-                room_result = Room.objects.filter(code=code)
-                if len(room_result) > 0:
-                    room = room_result[0]
-                    self.request.session['room_code'] = code 
-                    return Response({'message': "Room Joined"}, status=status.HTTP_200_OK)
+    lookup_url_kwarg = 'code'
 
-                return Response({'Bad Request': "invalid Room Code"}, status=status.HTTP_400_BAD_REQUEST)
-                
-            return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            room_result = Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
@@ -77,3 +80,13 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+            
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
